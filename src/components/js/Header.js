97bb860162bcabadb049f148ac699ac57be2c00b
Ayush <ayushpersonal2310@gmail.com>
2022-09-22 14:07:28 +0530

    Added Changes

--- src/components/js/Header.js
+++ src/components/js/Header.js
@@ -14,33 +14,33 @@ const Header = () => {
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0 ml-auto">
-                        <li className="nav-item navListItems">
+                        <li className="nav-item navListItems mx-4">
                             <Link className="nav-link text-white  " to="/">
                                 Home
                             </Link>
                         </li>
-                        <li className="nav-item navListItems">
+                        <li className="nav-item navListItems mx-4">
                             <Link className="nav-link text-white  " to="/news">
                                 News
                             </Link>
                         </li>
-                        <li className="nav-item navListItems">
+                        <li className="nav-item navListItems mx-4">
                             <Link className="nav-link text-white " to="/new-post">
                                 New Post
                             </Link>
                         </li>
-                        <li className="nav-item navListItems">
+                        <li className="nav-item navListItems mx-4">
                             <Link className="nav-link text-white " to="/activity">
                                 Activity
                             </Link>
                         </li>
-                        <li className="nav-item navListItems">
+                        <li className="nav-item navListItems mx-4">
                             <Link className="nav-link text-white " to="/profile">
                                 Profile
                             </Link>
                         </li>
                     </ul>
-                    <button type="submit" className="btn btn-success">
+                    <button type="submit" className="btn btn-success mx-4">
                         <Link className="text-white" to="/register">
                             Sign Up
                         </Link>
@@ -79,22 +79,22 @@ export default Header
 //                     </button>
 //                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
 //                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 navList">
-//                             <li className="nav-item navListItems">
+//                             <li className="nav-item navListItems mx-4">
 //                                 <Link className="nav-link text-white  " to="/">
 //                                     Home
 //                                 </Link>
 //                             </li>
-//                             <li className="nav-item navListItems">
+//                             <li className="nav-item navListItems mx-4">
 //                                 <Link className="nav-link text-white  " to="/news">
 //                                     News
 //                                 </Link>
 //                             </li>
-//                             <li className="nav-item navListItems">
+//                             <li className="nav-item navListItems mx-4">
 //                                 <Link className="nav-link text-white " to="/new-post">
 //                                     New Post
 //                                 </Link>
 //                             </li>
-//                             <li className="nav-item navListItems">
+//                             <li className="nav-item navListItems mx-4">
 //                                 <Link className="nav-link text-white " to="/activity">
 //                                     Activity
 //                                 </Link>
